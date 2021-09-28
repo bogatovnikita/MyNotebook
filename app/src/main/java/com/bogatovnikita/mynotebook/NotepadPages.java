9df4d572e46bd8b4cbdad6895b121ee741e51759
bogatovnikita <bogatovnikita@gmail.com>
2021-09-28 20:40:47 +0300

    added a menu button and handled its pressing

--- app/src/main/java/com/bogatovnikita/mynotebook/NotepadPages.java
+++ app/src/main/java/com/bogatovnikita/mynotebook/NotepadPages.java
@@ -1,14 +1,52 @@
 package com.bogatovnikita.mynotebook;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class NotepadPages extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notepad_pages);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);//заменяем штатный тулбар своим
+    }
+
+    /**
+     * методы для работы меню
+     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.notes_list_menu, menu);
+        return true;//возвращает истину, если меню создано
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.new_note_menu) {
+            openNewNote();
+            return true;//возвращает истину, если обработали нажатие
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openNewNote() {
+        Intent intent = new Intent(this, NotePage.class);
+        startActivity(intent);
     }
 }
