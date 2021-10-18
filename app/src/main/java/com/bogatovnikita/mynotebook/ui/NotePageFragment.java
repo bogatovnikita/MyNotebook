a8621ea2be053decaba3256836543988b8f7cfce
bogatovnikita <bogatovnikita@gmail.com>
2021-10-18 22:02:55 +0300

    add recyclerview

--- app/src/main/java/com/bogatovnikita/mynotebook/ui/NotePageFragment.java
+++ app/src/main/java/com/bogatovnikita/mynotebook/ui/NotePageFragment.java
@@ -4,14 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.bogatovnikita.mynotebook.R;
+import com.bogatovnikita.mynotebook.domain.NoteEntity;
+import com.bogatovnikita.mynotebook.domain.Repository;
 
 public class NotePageFragment extends Fragment {
+    Integer id;
+    EditText titleEditText;
+    EditText noteEditEditText;
 
     @Nullable
     @Override
@@ -19,4 +26,23 @@ public class NotePageFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_note_page_screen, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        titleEditText = view.findViewById(R.id.title_edit_text);
+        noteEditEditText = view.findViewById(R.id.note_edit_text);
+        if (getArguments() != null) {
+            id = getArguments().getInt("id");
+            titleEditText.setText(getArguments().getString("title"));
+            noteEditEditText.setText(getArguments().getString("noteText"));
+            Repository.repo.deleteNotes(id);
+        } else {
+            Button button = view.findViewById(R.id.save_note_button);
+            button.setOnClickListener(view1 -> {
+                String title = titleEditText.getText().toString();
+                String noteEdit = noteEditEditText.getText().toString();
+                Repository.repo.createNotes(new NoteEntity(id, title, noteEdit));
+            });
+        }
+    }
 }
--- app/src/main/java/com/bogatovnikita/mynotebook/ui/NotepadPagesFragment.java
+++ app/src/main/java/com/bogatovnikita/mynotebook/ui/NotepadPagesFragment.java
@@ -11,10 +11,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bogatovnikita.mynotebook.R;
+import com.bogatovnikita.mynotebook.domain.NoteEntity;
+import com.bogatovnikita.mynotebook.domain.Repository;
 
 public class NotepadPagesFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private NotesAdapter adapter = new NotesAdapter();
+    NotePageFragment notePageFragment = new NotePageFragment();
 
     @Nullable
     @Override
@@ -37,14 +44,36 @@ public class NotepadPagesFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.new_note_menu) {
-            NotePageFragment notePageFragment = new NotePageFragment();
             getActivity().getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_notepad_list_container, notePageFragment, "findFragmentNotePageScreen")
+                    .replace(R.id.fragment_notepad_list_container, notePageFragment)
                     .addToBackStack(null)
                     .commit();
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Repository.repo.createNotes(new NoteEntity(1, "sdfsf", "noteEdit"));
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener(this::onItemClick);
+        adapter.setData(Repository.repo.getNotes());
+    }
+
+    private void onItemClick(NoteEntity item) {
+        Bundle bundle = new Bundle();
+        bundle.putInt("id", item.getId());
+        bundle.putString("title", item.getTitle());
+        bundle.putString("noteText", item.getNoteText());
+        notePageFragment.setArguments(bundle);
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_notepad_list_container, notePageFragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
 }
 
